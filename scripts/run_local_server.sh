f01e0289410001f1429f3ea7fdd71b23ca2ff475
Phong Nguyen <contact@phong.jp>
2019-04-21 03:45:30 +0900

    Fix script bug, change site name

--- scripts/run_local_server.sh
+++ scripts/run_local_server.sh
@@ -1,6 +1,6 @@
 # kill jekyll server if it is already running
 if pids=$(sudo lsof -i:4000 -t); then
-    sudo kill $pids
+    sudo kill -9 $pids
 fi
 # start jeklyll server
 bundle exec jekyll serve
